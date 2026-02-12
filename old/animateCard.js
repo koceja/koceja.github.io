b2a60dae7e12cae88d00470f04011a6fcd3ccc48
Daniel Koceja <dankoceja@gmail.com>
2026-02-12 14:08:25 -0800

    Update website feb 2026

--- old/animateCard.js
+++
@@ -1,9 +0,0 @@
-$(".card").mouseenter(function () {
-  $(this).stop();
-  $(this).animate({ marginTop: "0px", marginBottom: "10px" }, "fast");
-});
-
-$(".card").mouseleave(function () {
-  $(this).stop();
-  $(this).animate({ marginTop: "5px", marginBottom: "5px"}, "fast");
-});
